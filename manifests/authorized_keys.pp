921b5f43cc5fffbe38256de493d498c180b0734a
Caius Durling <dev@caius.name>
2017-06-25 17:14:32 +0100

    Update live root ssh keys when changing them

--- manifests/authorized_keys.pp
+++ manifests/authorized_keys.pp
@@ -5,12 +5,19 @@ class usbkey_config::authorized_keys {
   require usbkey_config
 
   usbkey_config::setting { 'root_authorized_keys_file':
-    value => 'authorized_keys',
-    order => 40,
+    value  => 'authorized_keys',
+    order  => 40,
   }
 
   concat { 'usbkey_authorized_keys_file':
     path    => '/usbkey/config.inc/authorized_keys',
     ensure_newline => true,
+    notify => Exec['copy_root_authorized_keys_file'],
   }
+
+  exec { 'copy_root_authorized_keys_file':
+    command     => '/usr/bin/cp /usbkey/config.inc/authorized_keys /root/.ssh/authorized_keys',
+    refreshonly => true,
+  }
+
 }
